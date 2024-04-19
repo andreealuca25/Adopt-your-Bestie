79cf20681750164ca2071d5f0a4a0ca87f65ac2f
Begu Maria Florina <maria.begu.florina@gmail.com>
2024-04-19 20:18:14 +0300

    Example of a simple GET for Client.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
 import "./App.css";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Homepage from "./components/homepage/Homepage";
 
 function App() {
-  return (
-    <div>
-      <Homepage />
-    </div>
-  );
+  const [data, setData] = useState([]);
+
+      useEffect(() => {
+          axios.get('http://localhost:8080/api/data')
+              .then(response => {
+                  setData(response.data);
+              })
+              .catch(error => {
+                  console.error('Error:', error);
+              });
+      }, []);
+
+      return (
+        <div>
+            <p>{data}</p>
+        </div>
+      );
 }
 
 export default App;
