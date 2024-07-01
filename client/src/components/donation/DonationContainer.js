479b4962d8e2f187864467a5d471965e39c4bcef
Luca Andreea <lucaandreearaluca@yahoo.com>
2024-07-01 16:54:50 +0300

    add .env and update configuration

--- client/src/components/donation/DonationContainer.js
+++ client/src/components/donation/DonationContainer.js
@@ -1,32 +1,15 @@
 import NavBarFooterWrapper from "../wrapper/NavBarFooterWrapper";
 import DonationForm from "./DonationForm";
-import {Elements} from '@stripe/react-stripe-js';
-import {loadStripe} from '@stripe/stripe-js';
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
-
-export default function DonationContainer(){
-  const stripePromise = loadStripe('pk_live_51PHreUJoSBaYIuuhyF2usHOPiSPQ1YkdXOFjkzFjwbEtGA2gj1rxzVxsKkUK2IvpFSruFoszynVEmfGwc5jdfpVE00FgIgViDg');
+export default function DonationContainer() {
+  const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_API_KEY);
   return (
-<<<<<<< HEAD
-    <NavBarFooterWrapper topPadding="pt-6">
-      <DonationForm />
-    </NavBarFooterWrapper>
-=======
-<<<<<<< Updated upstream
-    <div>
-      <NavBar />
-      <div className="pt-6">
-        <DonationForm />
-        <Footer />
-      </div>
-    </div>
-=======
     <NavBarFooterWrapper topPadding="pt-6">
       <Elements stripe={stripePromise}>
         <DonationForm />
       </Elements>
     </NavBarFooterWrapper>
->>>>>>> Stashed changes
->>>>>>> client_BMF
   );
-};
+}
